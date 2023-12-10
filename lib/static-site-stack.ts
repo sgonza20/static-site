a84ac967163a5d0d52d05b49a848b90f1afdeb47
Sam Lozano <sgonza20@vols.utk.edu>
2023-12-09 22:24:01 -0500

    chore: linting fixture

--- lib/static-site-stack.ts
+++ lib/static-site-stack.ts
@@ -1,25 +1,25 @@
 #!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { StaticSite } from './static-site';
+import * as cdk from 'aws-cdk-lib'
+import { StaticSite } from './static-site'
 
 export class MyStaticSiteStack extends cdk.Stack {
-    constructor(parent: cdk.App, name: string, props: cdk.StackProps) {
-        super(parent, name, props);
+  constructor (parent: cdk.App, name: string, props: cdk.StackProps) {
+    super(parent, name, props)
 
-        new StaticSite(this, 'StaticSite', {
-            domainName: this.node.tryGetContext('domain'),
-            siteSubDomain: this.node.tryGetContext('subdomain'),
-        });
-    }
+    new StaticSite(this, 'StaticSite', {
+      domainName: this.node.tryGetContext('domain'),
+      siteSubDomain: this.node.tryGetContext('subdomain')
+    })
+  }
 }
 
-const app = new cdk.App();
+const app = new cdk.App()
 
 new MyStaticSiteStack(app, 'MyStaticSite', {
-    env: { 
-      account: process.env.CDK_DEFAULT_ACCOUNT, 
-      region: process.env.CDK_DEFAULT_REGION 
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION
   }
-});
+})
 
-app.synth();
+app.synth()
